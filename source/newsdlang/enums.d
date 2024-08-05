4c885584dc100348a8f7054719eab1fbdd4949b3
ZILtoid1991 <laszloszeremi@outlook.com>
2024-08-06 00:09:09 +0200

    First steps

--- source/newsdlang/enums.d
+++ source/newsdlang/enums.d
@@ -1,2 +1,28 @@
 module newsdlang.enums;
 
+@safe:
+
+enum DLElementType : ubyte {
+    init,
+    Tag,
+    Document,
+    Value,
+    Attribute,
+    Comment,
+}
+
+enum DLDocumentType : ubyte {
+    init,       ///Undefined/unknown
+    SDL,        ///Original SDLang specs
+    KDL,        ///KDL specification with its enhancements and limitations
+    XDL,        ///XDL specification (SDLang, but with better date and )
+}
+
+enum DLValueType : ubyte {
+    init,
+    Null,
+    Integer,
+    Float,
+    String,
+    Date,
+}
