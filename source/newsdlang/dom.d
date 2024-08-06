fb6d0e2835344caf7f32a00bee5544cebcc45ac2
ZILtoid1991 <laszloszeremi@outlook.com>
2024-08-06 23:38:10 +0200

    A bit of work on the DOM

--- source/newsdlang/dom.d
+++ source/newsdlang/dom.d
@@ -1,7 +1,13 @@
 module newsdlang.dom;
 
+import newsdlang.exceptions;
+
 @safe:
 
 public abstract class DLElement {
+    public DLElement[] _allChildElements;
     public abstract string toDLString();
+    public DLElement[] allChildElements() {
+        throw new DLException("This element does not support having any child elements");
+    }
 }
