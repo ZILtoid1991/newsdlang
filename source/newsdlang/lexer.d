419c3cef3eaf21b51031b1336e58535e0aa4ac24
ZILtoid1991 <laszloszeremi@outlook.com>
2024-12-29 21:45:59 +0100

    Documentation + attributes

--- source/newsdlang/lexer.d
+++ source/newsdlang/lexer.d
@@ -53,48 +53,60 @@ struct Lexer
     
 
     /// Returns true if position is hit the end of the input range
-    auto empty() const
+    auto empty() const @nogc nothrow
     {
         return pos >= input.length;
     }
 
     /// Sets the current position as the new mark
-    void start()
+    void start() @nogc nothrow
     {
         begin = pos;
     }
 
     /// Returns the current slice
-    string get() const
+    string get() const @nogc nothrow
     {
         return input[begin .. pos];
     }
     /// Peeks the current character.
-    char peek() const
+    char peek() const @nogc nothrow
     {
         return input[pos];
     }
     /// Peeks the current position for a string of size indicated by `am`.
     /// Returns the string, or null if string would be out of bounds.
-    string peek(size_t am) const
+    string peek(size_t am) const @nogc nothrow
     {
         if (pos + am > input.length) return null;
         return input[pos..pos + am];
     }
     /// Peeks the previous character.
-    char peekBack() const
+    char peekBack() const @nogc nothrow
     {
+        if (cast(sizediff_t)pos - 1 < 0)
+        {
+            return char.init;
+        }
         return input[pos - 1];
     }
+    char peekAhead() const @nogc nothrow
+    {
+        if (empty || pos + 1 >= input.length)
+        {
+            return char.init;
+        }
+        return input[pos];
+    }
     /// Steps one forward.
-    size_t step()
+    size_t step() @nogc nothrow
     {
         if (!empty) pos++;
         return pos;
     }
 
     /// Move forward until any of the characters are being encountered
-    void dropWhile(string s)
+    void dropWhile(string s) @nogc nothrow
     {
         while (pos < input.length && indexOf(s, input[pos]) != -1)
         {
@@ -165,7 +177,7 @@ struct Lexer
     }
     /// Advance forward if any of the characters in `s` is encountered.
     /// Steps forward an extra if `included` is true.
-    size_t advanceUntilAny(string s, bool included)
+    sizediff_t advanceUntilAny(string s, bool included)
     {
         enforce!LexerException(!empty, "No more characters are found!");
 
@@ -185,11 +197,14 @@ struct Lexer
 }
 
 unittest {
+    import std.stdio;
     string sdlangString = q"{
         foo "bar" 513
-        bar "baz" 0x56_4F
-        baz 8640.84
+        bar "baz" 0x56_4F {
+            baz 8640.84
+        }
         fish 2024-09-17T20:55:43Z
+        someTag "string with multiple spaces"
     }";
     Lexer testLexer;
     testLexer.setSource = sdlangString;
@@ -201,4 +216,20 @@ unittest {
     testLexer.start;
     assertNotThrown!LexerException(testLexer.advanceUntil('"', false));
     assert(testLexer.get == "bar");
+    // testLexer.step();
+    assertNotThrown!LexerException(testLexer.advanceUntil(' ', true));
+    testLexer.start;
+    assertNotThrown!LexerException(testLexer.advanceUntilAny(" \n\f\t", false));
+    assert(testLexer.get == "513");
+    testLexer.dropWhile(" \n\f\t");
+    testLexer.start;
+    assertNotThrown!LexerException(testLexer.advanceUntilAny(" \n\f\t", false));
+    assert(testLexer.get == "bar");
+    testLexer.dropWhile(" \n\f\t");
+    testLexer.start;
+    assert(testLexer.peek() == '\"');
+    testLexer.step();
+    testLexer.start;
+    assertNotThrown!LexerException(testLexer.advanceUntil('"', false));
+    assert(testLexer.get == "baz");
 }
